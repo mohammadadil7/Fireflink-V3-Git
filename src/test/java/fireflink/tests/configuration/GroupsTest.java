f67c444d0d4857b6e1f5765ed995a2513083749a
Mohammad Adil <mohammadadil7674@gmail.com>
2026-06-22 16:35:46 +0530

    suite test3

--- src/test/java/fireflink/tests/configuration/GroupsTest.java
+++ src/test/java/fireflink/tests/configuration/GroupsTest.java
@@ -138,16 +138,6 @@ public class GroupsTest extends BaseClass {
 
 	}
 	
-	@Test(description = "GR007_Verify allure")
-	public void GR007() throws Throwable
-
-	{
-	//	String recipientName = jsonUtils.getJsonValue(groupsTestdataFileName, "$.GR005.recipientName");
-	//	String recipietEmail = jsonUtils.getJsonValue(groupsTestdataFileName, "$.GR005.recipientEmail");
-
-		signPage.signToFireflink(email, password);
-		Assert.assertTrue(false);
-		
-	}
+	
 }
 
